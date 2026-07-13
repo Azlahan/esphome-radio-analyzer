c94a530ecac1ce5f48f299292e74149811f8ae32
Azlahan <masurier.jb@gmail.com>
2026-07-13 17:33:34 +0200

    Update radio_analyzer.h

--- components/radio_analyzer/radio_analyzer.h
+++ components/radio_analyzer/radio_analyzer.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace radio_analyzer {
+
+
+class RadioAnalyzer : public Component {
+
+ public:
+
+  void setup() override;
+  void loop() override;
+
+};
+
+
+}
+}
