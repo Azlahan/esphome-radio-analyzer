00e6b9821d3ff837dd823e17428d05eb361a38fd
Azlahan <masurier.jb@gmail.com>
2026-07-13 19:23:06 +0200

    Update radio_analyzer.cpp

--- components/radio_analyzer/radio_analyzer.cpp
+++ components/radio_analyzer/radio_analyzer.cpp
@@ -13,6 +13,18 @@ void RadioAnalyzer::setup() {
 
 void RadioAnalyzer::loop() {
 
+  if (millis() - last_update_ > 1000) {
+
+    last_update_ = millis();
+
+    float rssi = -100;
+
+    if (rssi_sensor_ != nullptr) {
+      rssi_sensor_->publish_state(rssi);
+    }
+
+  }
+
 }
 
 
