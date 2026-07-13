b25e077ae10b9ace98bb9f12568c9cd20b8f8461
Azlahan <masurier.jb@gmail.com>
2026-07-13 17:32:34 +0200

    Update __init__.py

--- components/radio_analyzer/__init__.py
+++ components/radio_analyzer/__init__.py
@@ -1,1 +1,17 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
 
+from esphome.const import CONF_ID
+
+
+radio_analyzer_ns = cg.esphome_ns.namespace("radio_analyzer")
+
+RadioAnalyzer = radio_analyzer_ns.class_(
+    "RadioAnalyzer",
+    cg.Component
+)
+
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(RadioAnalyzer),
+})
