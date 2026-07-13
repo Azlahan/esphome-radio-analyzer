274a991078d6e39aef4805bb1fece46dd16936d5
Azlahan <masurier.jb@gmail.com>
2026-07-13 19:19:29 +0200

    Update sensor.py

--- components/radio_analyzer/sensor.py
+++ components/radio_analyzer/sensor.py
@@ -1,1 +1,41 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
 
+from esphome.components import sensor
+
+from esphome.const import (
+    CONF_ID,
+    DEVICE_CLASS_SIGNAL_STRENGTH,
+    UNIT_DECIBEL,
+)
+
+
+radio_analyzer_ns = cg.esphome_ns.namespace("radio_analyzer")
+
+RadioAnalyzer = radio_analyzer_ns.class_(
+    "RadioAnalyzer",
+    cg.Component
+)
+
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(RadioAnalyzer),
+
+    cv.Optional("rssi"): sensor.sensor_schema(
+        unit_of_measurement=UNIT_DECIBEL,
+        accuracy_decimals=0,
+        device_class=DEVICE_CLASS_SIGNAL_STRENGTH,
+    ),
+})
+
+
+async def to_code(config):
+
+    var = cg.new_Pvariable(
+        config[CONF_ID]
+    )
+
+    await cg.register_component(
+        var,
+        config
+    )
