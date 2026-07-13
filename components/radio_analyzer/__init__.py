23daf0a7d2ceaeb7964688ab4a72e4845ce2ac99
Azlahan <masurier.jb@gmail.com>
2026-07-13 19:25:12 +0200

    Update __init__.py

--- components/radio_analyzer/__init__.py
+++ components/radio_analyzer/__init__.py
@@ -1,6 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
+from esphome.components import sensor
+
 from esphome.const import CONF_ID
 
 
@@ -12,6 +14,32 @@ RadioAnalyzer = radio_analyzer_ns.class_(
 )
 
 
+CONF_RSSI = "rssi"
+
+
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(RadioAnalyzer),
+
+    cv.Optional(CONF_RSSI): sensor.sensor_schema(
+        accuracy_decimals=0,
+        unit_of_measurement="dBm",
+    ),
 })
+
+
+async def to_code(config):
+
+    var = cg.new_Pvariable(
+        config[CONF_ID]
+    )
+
+    await cg.register_component(
+        var,
+        config
+    )
+
+    if CONF_RSSI in config:
+        sens = await sensor.new_sensor(config[CONF_RSSI])
+        cg.add(
+            var.set_rssi_sensor(sens)
+        )
