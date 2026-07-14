9f736e134cddc203c3ee42df58910c609bd5f4f9
Azlahan <masurier.jb@gmail.com>
2026-07-14 10:08:32 +0200

    Update radio_analyzer.h

--- components/radio_analyzer/radio_analyzer.h
+++ components/radio_analyzer/radio_analyzer.h
@@ -2,29 +2,26 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/sx127x/sx127x.h"
 
 namespace esphome {
 namespace radio_analyzer {
 
-class RadioAnalyzer : public Component {
-
+class RadioAnalyzer : public Component, public sx127x::SX127xListener {
  public:
-
   void setup() override;
-  void loop() override;
+  void dump_config() override;
 
   void set_rssi_sensor(sensor::Sensor *sensor) {
-    rssi_sensor_ = sensor;
+    this->rssi_sensor_ = sensor;
   }
 
+  void on_packet(const std::vector<uint8_t> &packet, float rssi, float snr) override;
+
  protected:
-
   sensor::Sensor *rssi_sensor_{nullptr};
-
-  unsigned long last_update_{0};
-
+  uint32_t packet_count_{0};
 };
 
-
-}
-}
+}  // namespace radio_analyzer
+}  // namespace esphome
