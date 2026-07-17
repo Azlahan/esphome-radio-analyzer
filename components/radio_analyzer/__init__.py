b7a5d0ff4e4c99f2f2246c8ee93f35d0e98b093c
Azlahan <masurier.jb@gmail.com>
2026-07-17 09:27:10 +0200

    Update __init__.py

--- components/radio_analyzer/__init__.py
+++ components/radio_analyzer/__init__.py
@@ -2,12 +2,14 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 
 from esphome.components import sensor, sx127x
+from esphome.const import (
+    CONF_ID,
+    DEVICE_CLASS_SIGNAL_STRENGTH,
+    UNIT_DECIBEL,
+)
 
-from esphome.const import CONF_ID
-
-
+# On indique qu'on dépend de sx127x et du domaine sensor d'ESPHome
 DEPENDENCIES = ["sensor", "sx127x"]
-
 
 radio_analyzer_ns = cg.esphome_ns.namespace("radio_analyzer")
 
@@ -16,46 +18,43 @@ RadioAnalyzer = radio_analyzer_ns.class_(
     cg.Component
 )
 
-
-CONF_RSSI = "rssi"
 CONF_RADIO_ID = "radio_id"
-
+CONF_RSSI = "rssi"
+CONF_SNR = "snr"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(RadioAnalyzer),
 
+    # Liaison avec le module radio
     cv.Required(CONF_RADIO_ID): cv.use_id(sx127x.SX127x),
 
+    # Capteur RSSI optionnel
     cv.Optional(CONF_RSSI): sensor.sensor_schema(
+        unit_of_measurement=UNIT_DECIBEL,
         accuracy_decimals=0,
-        unit_of_measurement="dBm",
+        device_class=DEVICE_CLASS_SIGNAL_STRENGTH,
+    ),
+
+    # Capteur SNR optionnel (très utile pour le SX127x en LoRa)
+    cv.Optional(CONF_SNR): sensor.sensor_schema(
+        unit_of_measurement="dB",
+        accuracy_decimals=1,
     ),
 })
 
-
 async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
 
-    var = cg.new_Pvariable(
-        config[CONF_ID]
-    )
+    # Récupération et assignation du module radio
+    radio = await cg.get_variable(config[CONF_RADIO_ID])
+    cg.add(var.set_radio(radio))
 
-    await cg.register_component(
-        var,
-        config
-    )
-
-    radio = await cg.get_variable(
-        config[CONF_RADIO_ID]
-    )
-
-    cg.add(
-        var.set_radio(radio)
-    )
-
-
+    # Configuration des capteurs si présents dans le YAML
     if CONF_RSSI in config:
-        sens = await sensor.new_sensor(config[CONF_RSSI])
+        sens_rssi = await sensor.new_sensor(config[CONF_RSSI])
+        cg.add(var.set_rssi_sensor(sens_rssi))
 
-        cg.add(
-            var.set_rssi_sensor(sens)
-        )
+    if CONF_SNR in config:
+        sens_snr = await sensor.new_sensor(config[CONF_SNR])
+        cg.add(var.set_snr_sensor(sens_snr))
